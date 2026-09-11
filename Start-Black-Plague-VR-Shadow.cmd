c5b1e43b0782b4640b660f5bc3cdbe16ed3d86a2
Oni <rubengarciaserrano88@gmail.com>
2026-09-11 17:53:31 +0200

    chore: keep shadow validation helper under tools

--- Start-Black-Plague-VR-Shadow.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0tools\Start-BlackPlagueShadowValidation.ps1" %*
-set "PVR_RESULT=%ERRORLEVEL%"
-if not "%PVR_RESULT%"=="0" pause
-exit /b %PVR_RESULT%
